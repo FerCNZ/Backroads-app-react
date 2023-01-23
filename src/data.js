8b93816b93ee0a470d1e126e32018d38433ebe1b
Fernando Carher <fercaraballo@hotmail.com>
2023-01-24 11:37:59 +1300

    update links

--- src/data.js
+++ src/data.js
@@ -13,10 +13,10 @@ export const pageLinks = [
 ];
 
 export const socialLinks = [
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 1, href: 'https://www.facebook.com', icon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
-  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
-  { id: 4, href: 'https://www.twitter.com', icon: 'fab fa-instagram' },
+  { id: 3, href: 'https://www.squarespace.com', icon: 'fab fa-squarespace' },
+  { id: 4, href: 'https://www.instagram.com', icon: 'fab fa-instagram' },
 ];
 
 export const services = [
